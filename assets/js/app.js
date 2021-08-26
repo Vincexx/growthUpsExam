bc89448ad4ca5d5195e4b25c7f14b6d0e7598c1e
cvpitagan <cvpitagan@gmail.com>
2021-08-26 21:41:18 +0800

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      count: 0,
+      users: [],
+      age: "all",
+      date: "",
+      convertedDate: "",
+    };
+  },
+  computed: {
+    formatDate() {
+      return moment(this.date).format("DD/MM/YYYY");
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async countMe() {
+      this.interval = setInterval(() => {
+        console.log(this.count);
+        this.count++;
+        if (this.count % 3 === 0) {
+          console.log("Foo");
+          this.count++;
+        }
+        if (this.count % 5 === 0) {
+          console.log("Bar");
+          this.count++;
+        }
+      }, 1000);
+    },
+    async convertDate() {
+      this.convertedDate = moment(this.date).format("YYYY-MM-DD");
+    },
+    async fetchUsers() {
+      await axios
+        .get("http://www.mocky.io/v2/5d73bf3d3300003733081869")
+        .then((response) => {
+          if (this.age === "all") {
+            this.users = [];
+            response.data.forEach((user) => {
+              this.users.push(user);
+            });
+          }
+
+          if (this.age === "below20") {
+            this.users = [];
+            response.data.forEach((user) => {
+              if (user.age <= 20) {
+                this.users.push(user);
+              }
+            });
+          }
+
+          if (this.age === "21-39") {
+            this.users = [];
+            response.data.forEach((user) => {
+              if (user.age > 20 && user.age < 40) {
+                this.users.push(user);
+              }
+            });
+          }
+
+          if (this.age === "40above") {
+            this.users = [];
+            response.data.forEach((user) => {
+              if (user.age >= 40) {
+                this.users.push(user);
+              }
+            });
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+});
+
+app.mount("#app");
